e083fad4b8690ea9dee1349a8f73beb9b0f000fc
Ankur Sundara <me@ankursundara.com>
2015-08-24 20:47:25 -0400

    Use material design theme for bootstrap and add some projects

--- js/main.js
+++ js/main.js
@@ -3,5 +3,17 @@
  */
 new WOW().init();
 $(function(){
-
+    $('.nav a').click(function(){
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('li').addClass("active");
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 1000);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
 });
